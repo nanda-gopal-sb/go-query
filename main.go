bfdcd9cd5bda78ba2727f6e8f5776dccf1b2224a
Nandagopal SB <nandagopal.nsb@gmail.com>
2025-03-29 17:10:58 +0530

    added functionalities

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v5"
 	"go-query/utils"
 	"log"
-	"os"
 )
 
-func connect() (*pgx.Conn, error) {
-	conn, err := pgx.Connect(context.Background(), "postgres://postgres:<password>du@localhost:5432/college_db")
+func connect(connString string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), connString)
 	if err != nil {
 		return nil, err
 	}
@@ -27,41 +25,46 @@ func queryData(conn *pgx.Conn, query string) {
 	defer rows.Close()
 	utils.PrintRowsAsTable(rows)
 }
+func getNewConnection() (newString string) {
+	newConn := &utils.Params{}
+	utils.GetParamsFromUser(newConn)
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", newConn.Username, newConn.Password, newConn.Host, newConn.Port, newConn.DB_name)
+	return connString
+}
 func showOptions(choice *int) {
 	fmt.Println("Welcome!")
 	fmt.Println("1 - Show all tables")
 	fmt.Println("2 - Make a query")
 	fmt.Println("3 - Show query history")
 	fmt.Println("4 - Clear Screen")
+	fmt.Println("5 - Add a connection")
 	fmt.Scanln(choice)
 }
 func main() {
 	var database *pgx.Conn
-	reader := bufio.NewReader(os.Stdin)
 	var choice int = 0
 	var err error
-	database, err = connect()
+	connString := getNewConnection()
+	database, err = connect(connString)
 	if err != nil {
 		fmt.Println("An error has occured while opening the database")
 		return
 	}
 	for true {
 		showOptions(&choice)
-		fmt.Println(choice)
 		if choice == 2 {
 			fmt.Println("Enter your query")
-			query, error := reader.ReadString('\n')
-			if error != nil {
-				fmt.Println("Error reading input:", error)
-				continue
-			}
+			query := utils.ReadLines()
 			queryData(database, query)
 			fmt.Println("")
 		} else if choice == 1 {
-
+			fmt.Println("")
 			queryData(database, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+			fmt.Println("")
 		} else if choice == 4 {
 			fmt.Print("\033[H\033[2J") //clears the screen, need to find concrete solution
+		} else if choice == 5 {
 		}
+
 	}
 }
--- utils/print.go
+++ utils/print.go
@@ -1,12 +1,52 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/jackc/pgx/v5"
 	"log"
+	"os"
 	"strings"
 )
 
+type Params struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	DB_name  string
+}
+
+func GetParamsFromUser(p *Params) error {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	Username, _ := reader.ReadString('\n')
+	p.Username = trimNewline(Username)
+
+	fmt.Print("Enter password: ")
+	password, _ := reader.ReadString('\n')
+	p.Password = trimNewline(password)
+
+	fmt.Print("Enter host: ")
+	host, _ := reader.ReadString('\n')
+	p.Host = trimNewline(host)
+
+	fmt.Print("Enter port: ")
+	port, _ := reader.ReadString('\n')
+	p.Port = trimNewline(port)
+
+	fmt.Print("Enter database name: ")
+	dbName, _ := reader.ReadString('\n')
+	p.DB_name = trimNewline(dbName)
+
+	return nil // No specific error handling in this basic example
+}
+
+// Helper function to remove the trailing newline character from input
+func trimNewline(s string) string {
+	return strings.TrimSuffix(s, "\n")
+}
 func PrintRowsAsTable(rows pgx.Rows) {
 	fieldDescriptions := rows.FieldDescriptions()
 	columnNames := make([]string, len(fieldDescriptions))
@@ -57,6 +97,22 @@ func PrintRowsAsTable(rows pgx.Rows) {
 	}
 }
 
+func ReadLines() string {
+	reader := bufio.NewReader(os.Stdin)
+	var lines []string
+	for {
+		line, err := reader.ReadString('\n') //this reads only one read
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(strings.TrimSpace(line)) == 0 {
+			break
+		}
+		line_s := strings.Split(line, " ")
+		lines = append(lines, line_s...)
+	}
+	return strings.Join(lines, " ")
+}
 func printHeaderDynamic(columnNames []string, columnWidths []int) {
 	for i, name := range columnNames {
 		fmt.Printf("%-*s  ", columnWidths[i], name)
